0207a220d77db91950382c40fe508eaf22704da5
vvc132003 <vvc132003@gmail.com>
2025-12-19 20:56:45 +0700

    .

--- src/app/config/api-urls.ts
+++ src/app/config/api-urls.ts
@@ -1,6 +1,6 @@
 export const API_URLS = {
-    api: 'http://192.168.1.6/api',
-    hub: 'http://192.168.1.6/signalrHub',
+    api: 'https://localhost:7274/api',
+    hub: 'https://localhost:7274/signalrHub',
     
     // hub: 'https://localhost:7159/api-cafeza/signalrHub',
 };
